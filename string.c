3ff38c2be7f9fcf64aaf5c0a99d7d8a2c6355dd2
Suleman Abdul Manan Wesoamo <sulemanabdulmanan@gmail.com>
2023-09-19 22:45:00 +0000

    Update string.c

--- string.c
+++ string.c
@@ -2,26 +2,26 @@
 
 /**
  * _strlen - returns the length of a string
- * @str: the string whose length to check
+ * @s: the string whose length to check
  *
  * Return: integer length of string
  */
-int _strlen(char *str)
+int _strlen(char *s)
 {
-	int length = 0;
+	int i = 0;
 
-	if (!str)
+	if (!s)
 		return (0);
 
-	while (*str++)
-		length++;
-	return (length);
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * _strcmp - performs lexicographic comparison of two strings.
- * @s1: the first string
- * @s2: the second string
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
@@ -45,7 +45,7 @@ int _strcmp(char *s1, char *s2)
  * @haystack: string to search
  * @needle: the substring to find
  *
- * Return: address of the next char of haystack or NULL
+ * Return: address of next char of haystack or NULL
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -60,16 +60,16 @@ char *starts_with(const char *haystack, const char *needle)
  * @dest: the destination buffer
  * @src: the source buffer
  *
- * Return: pointer to the destination buffer
+ * Return: pointer to destination buffer
  */
 char *_strcat(char *dest, char *src)
 {
-	char *result = dest;
+	char *ret = dest;
 
 	while (*dest)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
 	*dest = *src;
-	return (result);
+	return (ret);
 }
